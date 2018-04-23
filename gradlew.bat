6955b1ea5e4fae9e5a574e41a23fa2ceb704fcca
sayed24 <sayedelabd04@gmail.com>
2018-04-23 14:16:17 +0200

    update greadlew.bat to set proxy param

--- gradlew.bat
+++ gradlew.bat
@@ -22,7 +22,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
-
+set DEFAULT_JVM_OPTS=-Dhttps.proxyHost=10.102.0.3 -Dhttps.proxyPort=3128
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
